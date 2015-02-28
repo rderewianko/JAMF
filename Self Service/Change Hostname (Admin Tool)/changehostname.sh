f6139d58f1feb659b669f80cb6987900fbc9d999
rderewianko <rderewianko@pingidentity.com>
2015-02-27 18:52:15 -0800

    Adding a top I created to change the hostname, as part of our Small Suite of tools we call the "admin tools"

---
+++ Self Service/Change Hostname (Admin Tool)/changehostname.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+########################################################################
+# Created By: Ross Derewianko Ping Identity Corporation
+# Creation Date: Febuary, 2015 
+# Last modified: Febuary 27, 2015
+# Brief Description: Changes machine hostname
+########################################################################
+
+#check for cocoa dialogue & if not install it
+if [ -d "<location of Cocoa Dialogue>" ]; then
+	CD="<location of Cocoa Dialogue>/Contents/MacOS/CocoaDialog"
+else
+	echo "CocoaDialog.app not found installing" 
+	/usr/sbin/jamf policy -trigger cocoa
+fi
+
+########################################################################
+# Functions
+#######################################################################
+
+#asks for the new hostname & then call in the cleaner!
+function cdprompt() {
+	hostname=`"$CD" standard-inputbox --title "What is the new hostname" --informative-text "Please enter a hostname using the following format. 
+	-r for retina and -a for air"`
+
+	if [ "$hostname" == "2" ]; then
+		echo "user cancelled"
+		exit 1
+	fi
+	cleanhostname
+}
+
+#cleans the first two characters out (cocoaDialogue adds a 1 \n to the string value which we don't need.)
+function cleanhostname() {
+	hostname=${hostname:2}
+}
+
+#checks for a blank hostname, and if its blank prompt agian 
+
+function checkforblank() {
+	while [[ -z $hostname && {$hostname+1} ]]
+	do
+		cdprompt
+	done
+}
+
+function sethostname() {
+	scutil --set HostName $hostname
+	scutil --set ComputerName $hostname
+	scutil --set LocalHostName $hostname
+}
+
+########################################################################
+# Script
+########################################################################
+cdprompt
+checkforblank
+sethostname
